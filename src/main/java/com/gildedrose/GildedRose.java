657e1873df918f2000bddac26a8d5a194e6fa18a
PHYC <phil.h.y.chen@gmail.com>
2024-07-09 18:04:43 +0800

    Add more comments to method `updateQuality()`.

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -10,39 +10,38 @@ class GildedRose {
         this.items = items;
     }
 
-    public void updateQuality() {
-    	
-    	// Define the default strategies and value bound...
+    public void updateQuality() {    	
+    	// Define the default strategies and value upper bound.
     	Function<Item, Integer> defaultQualityDeltaStrategy = item -> (item.sellIn < 1) ? -2 : -1;
     	Integer defaultQualityMaxValue = 50;
     	Function<Item, Integer> defaultSellInStrategy = item -> -1;
     	
-    	// Register item-specific quality changing strategies here...
+    	// Register item-specific Quality changing strategies.
+    	// Each strategy should return only the per-day delta in Quality.
     	Map<String, Function<Item, Integer>> qualityDeltaStrategyRegistry = Map.of(
-    			"Aged Brie", item -> (item.sellIn < 1) ? 2 : 1,
-    			"Sulfuras, Hand of Ragnaros", item -> 0,
+    			"Aged Brie", item -> (item.sellIn < 1) ? 2 : 1, // "Aged Bries" increases in Quality instead.
+    			"Sulfuras, Hand of Ragnaros", item -> 0, // "Sulfuras" is a legendary item and as such its Quality is 80 and it never alters.
     			"Backstage passes to a TAFKAL80ETC concert", 
     				item -> (item.sellIn > 10) ? 1 : 
     						(item.sellIn > 5) ? 2 : 
-							(item.sellIn > 0) ? 3 : Integer.MIN_VALUE,
-				"Conjured Mana Cake", item -> (item.sellIn < 0) ? -4 : -2    										
+							(item.sellIn > 0) ? 3 : Integer.MIN_VALUE, // "Backstage passes" increases in Quality as its SellIn value approaches until expiry.  
+				"Conjured Mana Cake", item -> (item.sellIn < 0) ? -4 : -2 // "Conjured" items degrade in Quality twice as fast as normal items.    										
     			);
     		    	
-    	// Register item-specific quality max values here... 
+    	// Register item-specific Quality max values.
     	Map<String, Integer> qualityMaxValueRegistry = Map.of(
-    			"Sulfuras, Hand of Ragnaros", 80
+    			"Sulfuras, Hand of Ragnaros", 80 // "Sulfuras" is a legendary item and as such its Quality is 80 and it never alters.
     			);
-    	
 
-    	// Register item-specific sell-in changing strategies here...
+    	// Register item-specific Sell-In changing strategies.
+    	// Each strategy should return only the per-day delta in Sell-In.
     	Map<String, Function<Item, Integer>> sellInDeltaStrategyRegistry = Map.of(
-    			"Sulfuras, Hand of Ragnaros", item -> 0
+    			"Sulfuras, Hand of Ragnaros", item -> 0 //"Sulfuras", being a legendary item, never has to be sold or decreases in Quality
     			);
-    	
-    	// Execute update...
+    		
+    	// Execute update for each Item object...
         for (Item item : items) {
-
-        	// Update quality...
+        	// Update Quality...
         	int qualityMaxValue = qualityMaxValueRegistry
         			.getOrDefault(item.name, defaultQualityMaxValue);
         	int qualityDelta = qualityDeltaStrategyRegistry
@@ -50,12 +49,11 @@ class GildedRose {
 					.apply(item);
         	item.quality = Math.max(0, Math.min(qualityMaxValue, item.quality + qualityDelta));
         	
-        	// Update sellIn...
+        	// Update Sell-In...
         	int sellInDelta = sellInDeltaStrategyRegistry
         			.getOrDefault(item.name, defaultSellInStrategy)
         			.apply(item);
-        	item.sellIn += sellInDelta;
-        	
+        	item.sellIn += sellInDelta;        	
         }        	
     }
 }

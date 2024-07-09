1d9fc852f9fe0efe0fc141f577c5bc9a1bbce223
PHYC <phil.h.y.chen@gmail.com>
2024-07-09 16:49:03 +0800

    Refactor method `updateQuality()` in GildedRose.java, set pom.xml to compile in Java 21 and add required approved txt files.

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -1,5 +1,8 @@
 package com.gildedrose;
 
+import java.util.Map;
+import java.util.function.Function;
+
 class GildedRose {
     Item[] items;
 
@@ -8,55 +11,51 @@ class GildedRose {
     }
 
     public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
+    	
+    	// Register item-specific quality changing strategies here...
+    	Map<String, Function<Item, Integer>> qualityDeltaStrategyRegistry = Map.of(
+    			"Aged Brie", item -> (item.sellIn < 1) ? 2 : 1,
+    			"Sulfuras, Hand of Ragnaros", item -> 0,
+    			"Backstage passes to a TAFKAL80ETC concert", 
+    				item -> (item.sellIn > 10) ? 1 : 
+    						(item.sellIn > 5) ? 2 : 
+							(item.sellIn > 0) ? 3 : Integer.MIN_VALUE,
+				"Conjured Mana Cake", item -> (item.sellIn < 0) ? -4 : -2    										
+    			);
+    		    	
+    	// Register item-specific quality max values here... 
+    	Map<String, Integer> qualityMaxValueRegistry = Map.of(
+    			"Sulfuras, Hand of Ragnaros", 80
+    			);
+    	
 
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
+    	// Register item-specific sell-in changing strategies here...
+    	Map<String, Function<Item, Integer>> sellInDeltaStrategyRegistry = Map.of(
+    			"Sulfuras, Hand of Ragnaros", item -> 0
+    			);
+    	
+    	// Define the default strategies and value bound...
+    	Function<Item, Integer> defaultQualityDeltaStrategy = item -> (item.sellIn < 1) ? -2 : -1;
+    	Integer defaultQualityMaxValue = 50;
+    	Function<Item, Integer> defaultSellInStrategy = item -> -1;
+    	
+    	// Execute update...
+        for (Item item : items) {
 
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
-
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
-                }
-            }
-        }
+        	// Update quality...
+        	int qualityMaxValue = qualityMaxValueRegistry
+        			.getOrDefault(item.name, defaultQualityMaxValue);
+        	int qualityDelta = qualityDeltaStrategyRegistry
+        			.getOrDefault(item.name, defaultQualityDeltaStrategy)
+					.apply(item);
+        	item.quality = Math.max(0, Math.min(qualityMaxValue, item.quality + qualityDelta));
+        	
+        	// Update sellIn...
+        	int sellInDelta = sellInDeltaStrategyRegistry
+        			.getOrDefault(item.name, defaultSellInStrategy)
+        			.apply(item);
+        	item.sellIn += sellInDelta;
+        	
+        }        	
     }
 }
